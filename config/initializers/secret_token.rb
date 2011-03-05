6206bf6892bba5116f4bef49465d31d2a046dc96
Geoffrey Grosenbach <boss@topfunky.com>
2011-03-04 16:47:01 -0800

    First import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bigapp::Application.config.secret_token = '5ece4fc609decd2c19cbb8eab5e82e71e6c1832099f0521557140a8b5af0dbf060ae618abc99d82749fb107ffe885aa6e4703d1eaa19e7c4b16450185349cb5f'
